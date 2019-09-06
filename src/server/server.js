eade5698e616da26aaf5b58b38e058d0bf19f1e7
Brendan Cowley <b.cowley1220@gmail.com>
2019-09-06 19:40:38 -0400

    converted connection for package.json to absolute

--- src/server/server.js
+++ src/server/server.js
@@ -19,7 +19,7 @@ const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
 const TOKEN_PATH = "token.json";
 
 // Load client secrets from a local file.
-fs.readFile("credentials.json", (err, content) => {
+fs.readFile(__dirname + "/credentials.json", (err, content) => {
   if (err) return console.log("Error loading client secret file:", err);
   // Authorize a client with credentials, then call the Gmail API.
   authorize(JSON.parse(content), listLabels);
